7335356c88593ee99fe0de00be2aec9be71f6db0
saitou-star <program0719.saitou@gmail.com>
2023-11-14 14:36:47 +0900

    第4章はじめ

---
+++ code4_4.py
@@ -0,0 +1,13 @@
+is_awake = True
+count = 0
+
+while is_awake == True:
+    count += 1
+    print(f"ひつじが{count}匹")
+
+    key = input("もう眠りそうですか？(y/n) >>")
+
+    if key == "y":
+        is_awake = False
+
+print("おやすみなさい…")
